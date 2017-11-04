cfa49111e86ebdc5a65d80cffccc1dd4c526f456
Mariana Sickler <marianaleal_93@hotmail.com>
2017-11-04 15:20:20 +0000

    automatically backed up by learn

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { ConnectedRestaurantInput } from './components/RestaurantInput';
 import { ConnectedRestaurants } from './components/Restaurants';
+import { bindActionCreators } from 'redux'
 
 export class App extends Component {
   render() {
